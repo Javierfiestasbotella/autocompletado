7443cc742d889e2f34bdb279b1f145a136628a74
Your Name <javierfiestasbotella@gmail.com>
2022-06-14 00:11:21 +0200

    Autorrelleno3 para verificar nombre de usuario

--- autorelleno.py
+++ autorelleno.py
@@ -36,6 +36,20 @@ def autorelleno2(clave):
             except Error as ex:
                 print("Error al intentar la conexión: {0}".format(ex))'''
 
+def autorelleno3(n,m):
+    return f'''
+def usuario():
+        usser=input("Introduce un usuario : ")
+        if len(usser)<{n} or len(usser)>{m}:
+            print("El usuario debe tener entre {n} y {m} caracteres")
+            usuario()
+        elif usser.isalnum()==False:
+            print("Los valores del usurio deben ser únicamente letras o números")
+            usuario()
+        else:
+            print(True)
+        '''
+
 
 
 def completa_conexion(n):#TE AUTOCOMPLETA UNA CLASE CON LA CONEXXION A UNA BBDD PASANDO COMO ARGUMENTO NOMBRE DE LA CLASE
@@ -48,6 +62,13 @@ def completa_conexion(n):#TE AUTOCOMPLETA UNA CLASE CON LA CONEXXION A UNA BBDD 
 
 def completa_interfaz(n):#AUTOCOMPLETA LOS MÉTODOS DE INTERFAZ DE LA VARIABLE PRIVADA QUE PASAS COMO ARGUMENTO
     prueba01=pc.copy(autorelleno1(n))
+    keyboard = Controller()
+    with keyboard.pressed(Key.ctrl):
+        keyboard.press('v')
+        keyboard.release('v')
+
+def verifica_ususario(n,m):#AUTOCOMPLETA VERIFICACION DE USUARIO; SOLO PUEDEN SER NÚMEROS O LETRAS Y PASARLE POR ARGUMENTO EL MÍNIMO Y MÁXIMO DE CARACTERES QUE QUIERES QUE TENGA.
+    prueba01=pc.copy(autorelleno3(n,m))
     keyboard = Controller()
     with keyboard.pressed(Key.ctrl):
         keyboard.press('v')
